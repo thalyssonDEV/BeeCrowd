7af174018c06b616a72cd7c789a2ccca36ab5a4d
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-04-24 23:50:15 -0300

    Create 1004.cpp

---
+++ BeeCrowd C++/Iniciante/1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cin >> a;
+    cin >> b;
+
+    int PROD = a * b;
+
+    cout << "PROD = " << PROD << endl;
+    return 0;
+}
