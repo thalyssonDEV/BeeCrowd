55917cd05e86f05a0fae82c99e95ac96753a37fd
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-04-26 15:40:38 -0300

    Update 1041.py

--- BeeCrowd Python/Iniciante/1041.py
+++ BeeCrowd Python/Iniciante/1041.py
@@ -1,1 +1,24 @@
+def main():
+    x,y = input().split( )
+    x = float(x)
+    y = float(y)
 
+    if x == 0 and y == 0:
+        print("Origem")
+    elif x == 0:
+        print("Eixo Y")
+    elif y == 0:
+        print("Eixo X")
+    else:
+        if x > 0:
+            if y > 0:
+                print("Q1")
+            else:
+                print("Q4")
+        else:
+            if y > 0:
+                print("Q2")
+            else:
+                print("Q3")
+                
+main()
