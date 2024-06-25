d270852e278c1d81a933df151301b1017a6a8850
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-06-25 14:45:17 -0300

    Create 1059.cpp

---
+++ BeeCrowd C++/Iniciante/1059.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    for (1 ; i <= 100 ; i++){
+        if (i % 2 == 0){
+            cout << i << endl;
+        }
+    }
+}
