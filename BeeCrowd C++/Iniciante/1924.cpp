2236fa73692f190db018fdbf8a91ebbac4fd665d
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-06-26 10:41:53 -0300

    Create 1924.cpp

---
+++ BeeCrowd C++/Iniciante/1924.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int opcoes;
+    string curso,resultado;
+    
+    cin >> opcoes;
+    
+    for (int i = 0; i < opcoes; i++){
+        cin >> curso;
+    }
+    cout << "Ciencia da Computacao" << endl;
+    return 0;
+}
