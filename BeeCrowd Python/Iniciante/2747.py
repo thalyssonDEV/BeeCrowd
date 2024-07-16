ecb067ec26b68aa564bf11f68abbe3dd694d085a
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-07-16 13:28:36 -0300

    Create 2747.py

---
+++ BeeCrowd Python/Iniciante/2747.py
@@ -0,0 +1,13 @@
+def main():
+    print("-" * 39)
+    
+    for x in range(1):
+        print("|", " " * 35, "|")
+        print("|", " " * 35, "|")
+        print("|", " " * 35, "|")
+        print("|", " " * 35, "|")
+        print("|", " " * 35, "|")
+
+    print("-" * 39)
+
+main()
