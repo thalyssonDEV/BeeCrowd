a3dff201ff13670623c83f94df46c0bf9a7eac8c
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-10 15:52:51 -0300

    Update 1038.py

--- BeeCrowd Python/Iniciante/1038.py
+++ BeeCrowd Python/Iniciante/1038.py
@@ -1,19 +1,28 @@
+def check_leds(digit):
+  led_mapping = {
+      '0': 6,
+      '1': 2,
+      '2': 5,
+      '3': 5,
+      '4': 4,
+      '5': 5,
+      '6': 6,
+      '7': 3,
+      '8': 7,
+      '9': 6
+  }
+  return led_mapping.get(digit,0)
+
 def main():
-    item,qtd = map(int,input().split())
-    
-    if item == 1:
-        preço = 4
-    if item == 2:
-        preço = 4.5
-    if item == 3:
-        preço = 5
-    if item == 4:
-        preço = 2
-    if item == 5:
-        preço = 1.5
+  test_cases = int(input())
 
-    valor_total = preço * qtd
+  for _ in range(test_cases):
+      number = input()
 
-    print(f"Total: R$ {valor_total:.2f}")
-    
+      total_leds = 0
+      for digit in number:
+          total_leds += check_leds(digit)
+
+      print(total_leds, "leds")
+
 main()
