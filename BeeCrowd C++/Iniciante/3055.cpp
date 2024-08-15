a7043b05991c0cad16703018bf21a8f705b68de0
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-15 00:51:53 -0300

    Create 3055.cpp

---
+++ BeeCrowd C++/Iniciante/3055.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int nota_1,nota_2,media;
+
+  cin >> nota_1;
+  cin >> media;
+  
+  nota_2 = (media * 2) - nota_1;
+  cout << nota_2 << endl;
+
+  return 0;
+}
