73491d6ae7bc5dcf46c2c988fc960fff24b13efc
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-20 12:00:53 -0300

    Create 2057.cpp

---
+++ BeeCrowd C++/Iniciante/2057.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int hora_saida, tempo_viagem, fuso_horario, horario_chegada, horario_chegada_fuso;
+
+  cin >> hora_saida >> tempo_viagem >> fuso_horario;
+
+  horario_chegada = hora_saida + tempo_viagem;
+  if (horario_chegada > 23) {
+    horario_chegada -= 24;
+  }
+
+  horario_chegada_fuso = horario_chegada + fuso_horario;
+  if (horario_chegada_fuso <= 0) {
+    horario_chegada_fuso += 24;
+  }
+  
+  if (horario_chegada_fuso == 24) {
+    horario_chegada_fuso = 0;
+  }
+  cout << horario_chegada_fuso << endl;
+
+  return 0;
+}
