e8db40568b88da8a8b4d4742ad42f099f52c99e1
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-09-04 17:10:04 -0300

    Update play_numbers.cpp

--- BeeCrowd C++/play_numbers.cpp
+++ BeeCrowd C++/play_numbers.cpp
@@ -3,6 +3,15 @@
 #include <list>
 #include <cstdlib>
 
+void showMenu(){
+    printf("========== MENU ========\n");
+    printf("[ 1 ] - ADICIONAR NÚMEROS\n");
+    printf("[ 2 ] - REMOVER NÚMEROS\n");
+    
+    
+}
+
+
 // Função para limpar a tela
 void limparTela() {
 #ifdef _WIN32
@@ -13,10 +22,21 @@ void limparTela() {
 }
 
 
-void showList(std::list<int> &minhaLista) {
+void addNumbers(const std::list<int> &minhaLista, int &numerosToAdd) {
+    minhaLista.push_back(numerosToAdd);
+}
+
+
+void removeNumbers() {
+    
+}
+
+
+
+void showList(const std::list<int> &minhaLista) {
     int count{};
 
-    printf("Lista Atual --> ");
+    printf("\nLista Atual --> ");
     printf("[");
     for (int chr : minhaLista) {
 
@@ -27,7 +47,7 @@ void showList(std::list<int> &minhaLista) {
         }
         count++;
     }
-    printf("]\n");
+    printf("]\n\n");
 }
 
 int main() {
@@ -35,18 +55,22 @@ int main() {
     std::list<int> minhaLista{};
 
     printf("======= BEM VINDO AO MANIPULADOR DE LISTAS ========\n");
+    printf("===================================================\n\n");
 
     printf("Digite a Quantidade de Elementos da Lista:\n");
     scanf("%d", &tamanhoLista);
 
-    printf("Digite os Elementos Separados Por Espaço:\n");
+    printf("\nDigite os Elementos Separados Por Espaço:\n");
 
     for (int i{}; i < tamanhoLista; i++) {
 
         scanf("%d", &elementos);
         minhaLista.push_back(elementos);
     }
+    
     showList(minhaLista);
+    
+    showMenu();
 
     int choice = 1;
     while (choice != 0) {
@@ -56,18 +80,30 @@ int main() {
         switch (choice) {
 
             case 1:
-                int qtdNumeros;
+                int qtdNumerosToAdd, numerosToAdd;
 
                 printf("Quantos Números Deseja Adicionar:\n");
-                scanf("%d", &qtdNumeros);
+                scanf("%d", &qtdNumerosToAdd);
 
-                int numerosAdd;
-                for (int i = 0; i < qtdNumeros; i++) {
-                    scanf("%d", &numerosAdd);
-
-                    minhaLista.push_back(numerosAdd);
+                printf("Digite os Elementos Separados Por Espaço:\n");
+                for (int i{}; i < qtdNumerosToAdd; i++) {
+                    scanf("%d", &numerosToAdd);
+                    addNumbers(minhaLista, numerosToAdd);
                 }
                 showList(minhaLista);
+                break;
+                
+            case 2:
+                int qtdNumerosToRemove, numerosToRemove;
+                
+                printf("Quantos Números Deseja Remover:\n");
+                scanf("%d", &qtdNumerosToRemove);
+                
+                printf("Digite os Elementos Separados Por Espaço:\n");
+                for (int i{}; i < qtdNumerosToRemove; i++){
+                    scanf("%d", &numerosToRemove);
+                }
+                
         }
     }
 }
